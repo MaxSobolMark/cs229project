99daf7479f2af4b48a41b2c3fd63ec4728e0faa7
Max Sobol Mark <maxsobolmark@gmail.com>
2019-06-04 15:49:01 -0700

    Finished talgebra_polynomial_roots

--- generate_steps_dataset.py
+++ generate_steps_dataset.py
@@ -1,6 +1,9 @@
 import argparse
 import re # Regex for finding numbers
 import os
+import numpy as np
+import string
+import random
 
 from Naked.toolshed.shell import muterun_js
 
@@ -14,6 +17,8 @@ def main(original_dataset_directory, steps_dataset_directory, train_modes, start
 	print(original_dataset_directory, steps_dataset_directory, train_modes)
 	for mode in train_modes:
 		print('Generating {}\n'.format(mode))
+		generate_algebra_polynomial_roots(steps_dataset_directory, end_index - start_index)
+		print('\talgebra_polynomial_roots successful')
 		generate_numbers_div_remainder(original_dataset_directory, steps_dataset_directory, mode)
 		print('\tnumbers_div_remainder successful')
 		generate_polynomials_evaluate(original_dataset_directory, steps_dataset_directory, mode)
@@ -172,6 +177,73 @@ def generate_polynomials_simplify_power(original_dataset_directory, steps_datase
 
 					i += 1
 
+
+def generate_algebra_polynomial_roots(steps_dataset_directory, number_of_questions):
+	write_path = os.path.join(steps_dataset_directory, 'algebra__polynomial_roots.txt')
+	if os.path.exists(write_path):
+		print('TASK ALREADY EXISTS: algebra__polynomial_roots.txt\tIf you want to generate it again please erase the file :)')
+		return
+
+	with open(write_path, 'a', newline='\n') as write_file:
+		for i in range(number_of_questions):
+			number_of_roots = np.random.randint(2, 5)
+			roots = [int(np.random.exponential(8)) for i in range(number_of_roots)]
+			roots = np.sort(roots)
+
+			type_of_problem = np.random.choice(['factor', 'roots'])
+
+			roots_pre_and_suffixes = [
+				('Solve ', ' = 0 for $.'),
+				('Let ', ' = 0. What is $'),
+				('Let ', ' = 0. Calculate $'),
+				('What is $ in ', ' = 0?'),
+				('Find $ such that ', ' = 0.'),
+				('Suppose ', ' = 0. What is $?'),
+				('Determine $ such that ', ' = 0.'),
+				('Determine $ given that ', ' = 0.')
+			]
+			variable = random.choice(string.ascii_lowercase)
+			full_polynomial = generate_poly_from_roots(roots, variable)
+			question = None
+			if type_of_problem == 'factor':
+				question = 'Factor ' + full_polynomial + '.\n'
+			else:
+				pre_and_suffix = random.choice(roots_pre_and_suffixes)
+				question = pre_and_suffix[0].replace('$', variable) + full_polynomial + pre_and_suffix[1].replace('$', variable) + '\n'
+
+			factors = [
+				variable if root is 0 else (
+					'(' + variable + (' + ' if root < 0 else ' - ') + str(abs(root)) + ')'
+				)
+			for root in roots]
+
+			intermediate_steps = []
+			for i in range(number_of_roots - 1):
+				if i == number_of_roots - 2:
+					intermediate_steps.append(''.join(factors))
+				else:
+					rest = generate_poly_from_roots(roots[i+1:], variable)
+					intermediate_steps.append(''.join(factors[:i+1]) + '({}){}'.format(rest, SEPARATOR_CHAR))
+			
+			answer = ''.join(intermediate_steps).strip(SEPARATOR_CHAR)
+			print(question + answer)
+
+			write_file.write(question + answer + '\n')
+
+
+
+
+def generate_poly_from_roots(roots, variable):
+	number_of_roots = len(roots)
+	polynomial_coefficients = np.poly(roots)
+	polynomial_coefficients = [
+		' ' + ('+ ' if coefficient >= 0 else '- ') + str(abs(int(coefficient)))
+	for coefficient in polynomial_coefficients]
+	
+	polynomial = ''.join([
+		'{}{}**{}'.format(polynomial_coefficients[degree], variable, number_of_roots - degree)
+	for degree in range(number_of_roots + 1)])
+	return polynomial.strip(' +')[:-4]
 
 
 if __name__ == '__main__':
